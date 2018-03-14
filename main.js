120168f525ba1812ee07ff766c72ae5efc5df131
Hanna Landerus <you@example.com>
2018-03-14 10:44:01 +0100

    new logo and function to sort on price

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 fetchBlackPanther();
+/*variabel -all information från de olika listorna, getmarvel funktionen
+rad 89 specar den att de ska vara pricet*/
+let globalPrices = [];
 
 /******************** fetch information regarding Black Panther ********************/
 function fetchBlackPanther(blackPanther) {
-
     fetch(`https://gateway.marvel.com:443/v1/public/characters/1009187/comics?orderBy=issueNumber&apikey=639194d75fce87a0e941f417bbff6300
 `).then(function (response) {
             return response.json();
         })
         .then(function (getMarvel) {
             marvelInformation(getMarvel);
+            //globalPrices = getMarvel;
+            
         })
 
         .catch(function (error) {
@@ -25,6 +29,8 @@ function fetchSpiderMan(spiderMan) {
         })
         .then(function (getMarvel) {
             marvelInformation(getMarvel);
+            //globalPrices = getMarvel;
+            
         })
         .catch(function (error) {
             console.log(error);
@@ -40,6 +46,8 @@ function fetchHulk(hulk) {
         })
         .then(function (getMarvel) {
             marvelInformation(getMarvel);
+            //globalPrices = getMarvel;
+            
         })
         .catch(function (error) {
             console.log(error);
@@ -55,6 +63,7 @@ function fetchIronMan(ironMan) {
         })
         .then(function (getMarvel) {
             marvelInformation(getMarvel);
+            //globalPrices = getMarvel;
         })
         .catch(function (error) {
             console.log(error);
@@ -64,12 +73,12 @@ function fetchIronMan(ironMan) {
 
 /********** function to print informarion from fetch: title, information and image **********/
 function marvelInformation(getMarvel) {
-    console.log(getMarvel);
+    //console.log(getMarvel);
     const marvelInformationElement = document.getElementById('marvelInformation');
     let htmlBlock = '';
 
     for (i = 0; i < getMarvel.data.results.length; i++) {
-        console.log(getMarvel.data.results[i].title)
+        // console.log(getMarvel.data.results[i].title)
         htmlBlock += `
     <h1>${getMarvel.data.results[i].title}</h1>
     <p>${getMarvel.data.results[i].description}</p>
@@ -77,42 +86,44 @@ function marvelInformation(getMarvel) {
     <img src="${getMarvel.data.results[i].thumbnail.path}.jpg"/>
     
       `;
-        /* kan tas bort*/
-        if(getMarvel.data.results[i].description === null)
-        {
-        console.log("no description");
-        }
-            
-
-    }
     
+       globalPrices.push(getMarvel.data.results[i].prices[0].price);
+    }
+
+
     marvelInformationElement.innerHTML = htmlBlock;
 }
+
+/************ funktion för att sortera på priserna när du trycker på knappen ************/
+const priceButton = document.getElementById('sortOnPrice');
+        priceButton.addEventListener('click', function () {
+            console.log("klick");
+             console.log(globalPrices);
+            
+           function compareNumbers (a,b) {
+                /*sorterar efter lägsda*/
+                  return a - b;
+               
+            }
+           
+            globalPrices.sort(compareNumbers);
+            
+        });
 
 /******************** input, show list depending on search input ********************/
 
 const search = document.getElementById('search');
-    search.addEventListener('change', function () {
-        const searchValue = search.value;
-        console.log(searchValue);
-        if (searchValue == "spider-man" || searchValue == "Spider Man" || searchValue == "spider man" || searchValue == "Spider man" ) {
-            fetchSpiderMan();
-        }else if(searchValue == "black panther" || searchValue == "Black Panther" || searchValue == "Black panther"){
-            fetchBlackPanther();
-        }else if(searchValue == "hulk" || searchValue == "Hulk"){
-            fetchHulk();
-        }else if(searchValue == "iron man" || searchValue == "Iron Man" || searchValue == "Iron man"){
-            fetchIronMan();
-        }
-
-    })
-const priceButton = document.getElementById('sortOnPrice');
- priceButton.addEventListener('click', function () {
-    for ( i = 0; i > 0; i++){
-        
+search.addEventListener('change', function () {
+    const searchValue = search.value;
+    //console.log(searchValue);
+    if (searchValue == "spider-man" || searchValue == "Spider Man" || searchValue == "spider man" || searchValue == "Spider man") {
+        fetchSpiderMan();
+    } else if (searchValue == "black panther" || searchValue == "Black Panther" || searchValue == "Black panther") {
+        fetchBlackPanther();
+    } else if (searchValue == "hulk" || searchValue == "Hulk") {
+        fetchHulk();
+    } else if (searchValue == "iron man" || searchValue == "Iron Man" || searchValue == "Iron man") {
+        fetchIronMan();
     }
-        
- })
- 
- 
- 
+
+})
